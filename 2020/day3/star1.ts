1904e22964eb6f4321caed402aab3ad6005364bd
Meir Cohen <meirkoen@gmail.com>
2020-12-03 08:43:29 +0200

    day3 star2

--- 2020/day3/star1.ts
+++ 2020/day3/star1.ts
@@ -1,12 +1,11 @@
 const input = require('./input');
 
-const getCountOfTrees = (input) => {
+const getCountOfTrees = (input, stemDown, stepRight) => {
   let row = 0, col = 0, counter = 0;
 
   while (row < input.length - 1) {
-    console.log(input[row][col]);
-    row++;
-    col += 3;
+    row += stemDown;
+    col += stepRight;
     col = col % input[row].length;
 
     if (input[row][col] === '#') {
@@ -17,5 +16,15 @@ const getCountOfTrees = (input) => {
   return counter;
 }
 
-const res = getCountOfTrees(input);
-console.log(res);
+const res1 = getCountOfTrees(input, 1, 1);
+const res2 = getCountOfTrees(input, 1, 3);
+const res3 = getCountOfTrees(input, 1, 5);
+const res4 = getCountOfTrees(input, 1, 7);
+const res5 = getCountOfTrees(input, 2, 1);
+
+console.log(res1);
+console.log(res2);
+console.log(res3);
+console.log(res4);
+console.log(res5);
+console.log(res1*res2*res3*res4*res5);
